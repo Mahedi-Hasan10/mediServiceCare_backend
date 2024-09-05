ae63993832b32e3b35ca19cd97013ab4b8494e28
Mahedi-Hasan10 <hasanmdmahedi193@gmail.com>
2024-09-06 00:09:35 +0600

    testimonial

--- src/controllers/testimonial.controller.js
+++ src/controllers/testimonial.controller.js
@@ -27,4 +27,52 @@ const createTestimonials = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, testimonial, "Testimonial Created Successfully"))
 })
 
-export { createTestimonials }
+const allTestimonial = asyncHandler(async (req, res) => {
+    const data = await Testimonial.find()
+    if (!data) throw new ApiError(400, "Server Error!")
+    return res.status(200)
+        .json(new ApiResponse(200, data, "Testimonial fetched successfully"))
+
+})
+
+const deleteTestimonials = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    if (!id) throw new ApiError(400, "Testimonial Id is required!")
+    const testimonial = await Testimonial.findById(id)
+    if (!testimonial) throw new ApiError(400, "Testimonial Already Deleted!")
+    await Testimonial.findByIdAndDelete(id)
+    return res.status(200)
+        .json(new ApiResponse(200, "Testimonial deleted Successfully"))
+})
+
+const updateTestimonial = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { client, designation, message } = req.body;
+
+    if (!id) throw new ApiError(400, "Testimonial Id is required!");
+
+    const testimonial = await Testimonial.findById(id);
+    if (!testimonial) throw new ApiError(400, "Testimonial not found or already deleted!");
+
+    const avatarLocalPath = req.file?.path;
+    let avatar = testimonial.avatar;
+
+    if (avatarLocalPath) {
+        const newAvatar = await uploadOnCloudinary(avatarLocalPath);
+        if (!newAvatar) throw new ApiError(500, "Error uploading avatar");
+        avatar = newAvatar?.url;
+    }
+
+    testimonial.client = client || testimonial.client;
+    testimonial.designation = designation || testimonial.designation;
+    testimonial.message = message || testimonial.message;
+    testimonial.avatar = avatar;
+
+    await testimonial.save();
+
+    return res.status(200).json(
+        new ApiResponse(200, testimonial, "Testimonial updated successfully")
+    );
+});
+
+export { createTestimonials, deleteTestimonials, allTestimonial, updateTestimonial }
--- src/routes/testimonial.routes.js
+++ src/routes/testimonial.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createTestimonials } from "../controllers/testimonial.controller.js";
+import { allTestimonial, createTestimonials, deleteTestimonials, updateTestimonial } from "../controllers/testimonial.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
-router.route("/").post(verifyJWT, upload.single("avatar"), createTestimonials)
+router.route("/").post(verifyJWT, upload.single("avatar"), createTestimonials).get(verifyJWT, allTestimonial)
+router.route("/:id").delete(verifyJWT, deleteTestimonials).patch(verifyJWT, upload.single("avatar"), updateTestimonial)
 export default router
